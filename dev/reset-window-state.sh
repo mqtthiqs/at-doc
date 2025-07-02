289bfd3971c8c5ee91516bacd0958ef9521ff0f3
Lucijan Busch <luci@seequence.org>
2025-07-02 09:36:36 +0200

    reset-window-state: add properties for "Carrousel" profile

--- dev/reset-window-state.sh
+++ dev/reset-window-state.sh
@@ -2,5 +2,23 @@
 
 # resets settings that influence screenshots
 
-cat "$HOME/Library/Application Support/com.INAgrm.GRMTools.Atelier.settings" | grep -v scaleFactor | grep -v modulationPaneHeight | grep -v windowState > "/tmp/com.INAgrm.GRMTools.Atelier.settings"
-mv "/tmp/com.INAgrm.GRMTools.Atelier.settings" "$HOME/Library/Application Support/com.INAgrm.GRMTools.Atelier.settings" 
+filename=com.INAgrm.GRMTools.Atelier.settings
+path="$HOME/Library/Application Support/$filename"
+
+cat "$path" | grep -v scaleFactor | grep -v modulationPaneHeight | grep -v windowState | grep -v "</PROPERTIES>" > "/tmp/$filename"
+mv "/tmp/$filename" "$path"
+
+if [ "$1" = "--profile" ]; then
+  if [ "$2" = "Carrousel" ]; then
+   cat >> "$path" <<EOF
+  <VALUE name="scaleFactor" val="0.5000000596046448"/>
+  <VALUE name="modulationPaneHeight" val="74.2503%"/>
+  <VALUE name="windowState" val="200 200 2400 1076"/>
+EOF
+  else
+   echo "Unknown profile"
+   exit 1
+  fi
+fi
+
+echo "</PROPERTIES>" >> "$path"
